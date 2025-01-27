7e39e37982bd0193058942a6b2b67757776add73
Junhyeong Jo <junhyeong.tech@gmail.com>
2025-01-27 23:06:43 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250127/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+
+int main() {
+    cin >> n >> m;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cout << 1;
+        }
+        cout << endl;
+    }
+
+    // Write your code here!
+
+    return 0;
+}
