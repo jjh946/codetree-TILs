0b6e86b8e7b7acbaafd8b7acfb1e046117fcd0c1
Junhyeong Jo <junhyeong.tech@gmail.com>
2025-02-26 21:47:47 +0900

    Title: 행복한 수열의 개수, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 250226/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 250226/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -17,14 +17,18 @@ int main() {
     // Please write your code here.
     for (int i = 1; i <= n; i++){
         int mx = 1;
+        int tmp = 1;
         for (int j = 1; j <= n; j++){
             if (grid[i][j]==grid[i][j-1]){
-                mx++;
+                tmp++;
+                if(tmp > mx){
+                    mx = tmp;
+                }
             }
             else{
-                mx = 1;
+                tmp = 1;
             }
-
+            
         }
         if(mx >= m){
             ans++;
@@ -34,12 +38,16 @@ int main() {
     }
     for (int i = 1; i <= n; i++){
         int mx = 1;
+        int tmp = 1;
         for (int j = 1; j <= n; j++){
             if (grid[j][i]==grid[j-1][i]){
-                mx++;
+                tmp++;
+                if(tmp>mx){
+                    mx = tmp;
+                }
             }
             else{
-                mx = 1;
+                tmp = 1;
             }
 
         }
