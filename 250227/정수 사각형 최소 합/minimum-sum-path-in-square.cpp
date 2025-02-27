5963d8e283e8190335b60a73f48d6d17070460ac
Junhyeong Jo <junhyeong.tech@gmail.com>
2025-02-27 11:21:20 +0900

    Title: 정수 사각형 최소 합, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 250227/정수 사각형 최소 합/minimum-sum-path-in-square.cpp
+++ 250227/정수 사각형 최소 합/minimum-sum-path-in-square.cpp
@@ -15,11 +15,12 @@ int main() {
     }
 
     // Please write your code here.
-    fill(&dp[0][0], &dp[0][0] + 103*103, 1000001);
+    fill(&dp[0][0], &dp[0][0] + 103*103, 1e9);
+    dp[1][n] = grid[1][n];
     for (int i = 1; i <= n; i++) {
         for (int j = n; j >= 1; j--) {
-            dp[1][n] = grid[1][n];
-            if (grid[i][j]>0){
+            
+            if (dp[i-1][j] < 100000000 || dp[i][j+1] < 100000000){
                 dp[i][j] = min(dp[i-1][j],dp[i][j+1]) + grid[i][j];
             }
             
