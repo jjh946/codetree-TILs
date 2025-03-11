1c0c589ec39ff27520b3f88ba46d167017d9935b
Junhyeong Jo <junhyeong.tech@gmail.com>
2025-03-11 23:45:14 +0900

    Title: Time to Time, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250311/Time to Time/time-to-time.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b, c, d;
+
+int main() {
+    cin >> a >> b >> c >> d;
+
+    // Please write your code here.
+    cout << 60*c + d - (60*a + b) << endl;
+    
+
+    return 0;
+}
