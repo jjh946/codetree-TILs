4457ef361e3dd5a69b78a078cf22881496d427c7
Junhyeong Jo <junhyeong.tech@gmail.com>
2025-03-24 22:43:41 +0900

    Title: 2개의 사탕, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250324/2개의 사탕/two-candies.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int grid[20][20];
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
