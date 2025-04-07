b037019c4bb279c016ffdc197461fe364dbbfe6f
Junhyeong Jo <junhyeong.tech@gmail.com>
2025-04-07 14:39:46 +0900

    Title: K번 최댓값으로 이동하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250407/K번 최댓값으로 이동하기/move-to-max-k-times.cpp
+++ 250407/K번 최댓값으로 이동하기/move-to-max-k-times.cpp
@@ -13,10 +13,6 @@ queue<pair<int,int>> q;
 int X;
 int mx;
 
-bool keepSearch = true;
-
-
-
 void Push(int x, int y){
     visited[x][y] = true;
     q.push(make_pair(x,y));
@@ -34,15 +30,15 @@ void bfs(int x, int y){
         }
     }
     
-    // 큐 초기화 (이전 BFS에서 남은 요소들 제거)
+    // 큐 초기화
     while(!q.empty()) q.pop();
-
+    
     int dx[4] = {-1,1,0,0};
     int dy[4] = {0,0,-1,1};
 
     Push(x,y);
     X = grid[x][y];
-    mx = 0;
+    mx = -1; // -1로 초기화하여 아무것도 찾지 못할 경우를 대비
 
     while(!q.empty()){
         pair<int,int> cur_pos = q.front();
@@ -51,18 +47,18 @@ void bfs(int x, int y){
         int cur_x = cur_pos.first;
         int cur_y = cur_pos.second;
 
-        for(int i=0;i<4;i++){
+        for(int i=0; i<4; i++){
             int next_x = cur_x + dx[i];
             int next_y = cur_y + dy[i];
 
             if(CanGo(next_x,next_y)) {
                 Push(next_x,next_y);
-                mx = max(mx, grid[next_x][next_y]);
+                if(grid[next_x][next_y] > mx) {
+                    mx = grid[next_x][next_y];
+                }
             }
         }
     }
-    
-
 }
 
 int main() {
@@ -76,25 +72,35 @@ int main() {
 
     cin >> r >> c;
 
-    
-    for(int it=1;it<=k;it++){
+    for(int it=1; it<=k; it++){
         bfs(r,c);
-
-        if(mx == 0) break;
-
-        bool found = false;
-        for (int i = 1; i <= n && !found; i++) {
-            for (int j = 1; j <= n && !found; j++) {
-                if(grid[i][j]==mx && keepSearch){
-                    r = i;
-                    c = j;
-                    found = true;
+        
+        // 갈 수 있는 곳이 없으면
+        if(mx == -1) {
+            break; // 또는 현재 위치 유지
+        }
+        
+        // 최대값을 갖는 위치 중 행이 가장 작고, 행이 같다면 열이 가장 작은 위치 선택
+        int new_r = n+1, new_c = n+1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if(grid[i][j] == mx && visited[i][j]) {
+                    if (i < new_r || (i == new_r && j < new_c)) {
+                        new_r = i;
+                        new_c = j;
+                    }
                 }
             }
         }
+        
+        // 새로운 위치로 이동
+        if(new_r <= n && new_c <= n) {
+            r = new_r;
+            c = new_c;
+        }
     }
+    
     cout << r << ' ' << c << '\n';
-
 
     return 0;
 }
