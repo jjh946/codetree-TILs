173b5fc720a71a82f93e1e0265677d8b6ec4d74c
Junhyeong Jo <junhyeong.tech@gmail.com>
2025-04-15 23:37:42 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250415/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int grid[103][103];
+bool visited[103][103];
+//int answer[100][100];
+int order;
+
+void dfs(int x, int y){
+    int dx[2] = {1,0};
+    int dy[2] = {0,1};
+
+    for(int i=0;i<2;i++){
+        int next_x = x + dx[i];
+        int next_y = y + dy[i];
+        if(next_x < n && next_y < m && grid[next_x][next_y] && !visited[next_x][next_y]) {
+            //answer[next_x][next_y] = order++;
+            visited[next_x][next_y] = true;
+            dfs(next_x, next_y);
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    // Please write your code here.
+    //answer[0][0] = order++;
+    visited[0][0] = true;
+    dfs(0,0);
+
+    cout << (visited[n-1][m-1] ? 1:0) << endl;
+
+    return 0;
+}
