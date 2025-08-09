ed33b4944b551646dbee601b00341691a7fcfb46
Junhyeong Jo <junhyeong.tech@gmail.com>
2025-08-09 17:06:13 +0900

    Title: 돌 잘 치우기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250809/돌 잘 치우기/clear-stones-well.cpp
+++ 250809/돌 잘 치우기/clear-stones-well.cpp
@@ -9,6 +9,7 @@ int n, k, m;
 int grid[100][100];
 int r[10000], c[10000];
 bool visited[100][100];
+//bool visited2[100][100];
 
 
 int mx;
@@ -19,6 +20,8 @@ queue<pair<int,int>> q;
 void Push(int x, int y){
     q.push(make_pair(x,y));
     visited[x][y] = true;
+    //visited2[x][y] = true;
+    
 }
 
 bool CanGo(int x, int y){
@@ -27,10 +30,6 @@ bool CanGo(int x, int y){
 
 //이게 내 딴짓이고 나는 이게 재미있다. 나쁘지 않다. 그냥 하면 되는 거니까 ㅋㅋ
 int bfs(int x, int y){
-
-    // visited 초기화
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++) visited[i][j]=false;
 
     int dx[4] = {-1,1,0,0};
     int dy[4] = {0,0,-1,1};
@@ -60,9 +59,17 @@ int bfs(int x, int y){
 void backtracking(int dol_cnt){
     // 여기서 돌 2개가 선택되면 bfs를 진행하도록 한다.
     if(dol_cnt==m){
+        // visited 초기화
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++) visited[i][j]=false;
+
+        int temp=0;
         for(int i=0;i<k;i++){
-            mx = max(mx, bfs(r[i],c[i]));
+            if(!visited[r[i]][c[i]]){
+                temp += bfs(r[i],c[i]);
+            }
         }
+        mx = max(mx, temp);
         return;
     }
     //이제 그리드를 순회하면서 돌을 치워줘보자
