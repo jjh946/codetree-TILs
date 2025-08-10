f850c424b6a1e304cb2751f3406b995ec00602b2
Junhyeong Jo <junhyeong.tech@gmail.com>
2025-08-10 09:23:11 +0900

    Title: 계단 오르기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250810/계단 오르기/climbing-stairs.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+int dp[1005];
+
+int main() {
+    cin >> n;
+
+    dp[1]=0;
+    dp[2]=1;
+    dp[3]=1;
+    for(int i=4;i<=n;i++){
+        dp[i]=dp[i-2] + dp[i-3];
+    }
+
+    cout << dp[n];
+
+    return 0;
+}
